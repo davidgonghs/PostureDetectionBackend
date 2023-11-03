0a302411d8df8f1e4344bd7a923e26ea4f7e2f50
David Gong <davidgonghongshen@gmail.com>
2023-11-03 16:46:11 +0800

    1

---
+++ src/main/java/com/posturedetetion/PostureDetetionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.posturedetetion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostureDetetionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostureDetetionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posturedetetion/PostureDetetionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posturedetetion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostureDetetionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
